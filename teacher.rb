af31b4b7752431bb0cd3f105c813fcdb81ddde58
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-04-17 14:40:15 +0100

    Create class teacher

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+relative 'person'
+
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(specialization, age, name = 'Unknow', parent_permission: true)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
