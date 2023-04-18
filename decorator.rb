045210f97fc179c36411d5ac7c0c3c18cc8c4ecf
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-04-18 14:15:42 +0100

    Capitalize Decorator class

--- decorator.rb
+++ decorator.rb
@@ -12,3 +12,18 @@ class Decorator < Nameable
     @nameable.correct_name
   end
 end
+
+# Capitalize Decorator class
+class CapitalizeDecorator < Decorator
+  def correct_name
+    nameable.correct_name.capitalize
+    end
+end
+
+# # Trimmer Decorator class
+# class TrimmerDecorator < Decorator
+#   def correct_name
+#     name = nameable.correct_name
+#     name.length > 10 ? name [0, 10] : name
+#     end
+#     end
