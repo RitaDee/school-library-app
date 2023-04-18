6fc43278b062f4a2b07faca694972b8ff7a4b996
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-04-18 14:16:04 +0100

    Trimmer Decorator class

--- decorator.rb
+++ decorator.rb
@@ -20,10 +20,10 @@ class CapitalizeDecorator < Decorator
     end
 end
 
-# # Trimmer Decorator class
-# class TrimmerDecorator < Decorator
-#   def correct_name
-#     name = nameable.correct_name
-#     name.length > 10 ? name [0, 10] : name
-#     end
-#     end
+# Trimmer Decorator class
+class TrimmerDecorator < Decorator
+  def correct_name
+    name = nameable.correct_name
+    name.length > 10 ? name [0, 10] : name
+    end
+    end
