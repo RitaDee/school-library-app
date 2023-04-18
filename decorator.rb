ba7c521093cafe82f62435f3cc866af1a68ab138
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-04-18 14:07:47 +0100

    Update nameable.rb file

--- decorator.rb
+++ decorator.rb
@@ -1,15 +1,14 @@
 require './nameable'
 
 class Decorator < Nameable
-    attr_accessor :nameable
+  attr_accessor :nameable
 
-    def initialize(nameable)
-        @nameable = nameable
-    end
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
 
-    def correct_name
-        @nameable.correct_name
-    end
-    end
-
-    
+  def correct_name
+    @nameable.correct_name
+  end
+end
--- nameable.rb
+++ nameable.rb
@@ -1,4 +1,5 @@
 class Nameable
-
-    def correct_name
-        raise NotImplementedError
+  def correct_name
+    raise NotImplementedError, 'Subclass must implement correct_name method'
+  end
+end
