5d6bdb736cd4b0bc0473cc124487031f0a65d3d1
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-04-19 14:55:28 +0100

    Fix linter error

--- rental.rb
+++ rental.rb
@@ -2,13 +2,13 @@ require './book'
 require './person'
 
 class Rental
-    attr_accessor :date, :book, :person
+  attr_accessor :date, :book, :person
 
-    def initialize(date, book, person)
-        @date = date
-        @book = book
-        @person = person
-        book.rentals << self
-        person.rentals << self
-    end
-    end
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    @person = person
+    book.rentals << self
+    person.rentals << self
+  end
+end
