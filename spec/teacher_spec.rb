413b45b149cbbfbb0ad51e7a938cb070e39001ca
Marc raphael <marcraphael979@gmail.com>
2023-04-27 10:15:25 +0100

    Check if the method Teacher can instancite an object

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -4,4 +4,10 @@ describe Teacher do
   before :each do
     @teacher = Teacher.new('CS', 30, 'John Doe')
   end
+
+  context 'given arguments' do
+    it 'instantiate an object ' do
+      expect(@teacher).to be_instance_of(Teacher)
+    end
+  end
 end
