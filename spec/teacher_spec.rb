052ed90d51298d3611f60a446e328ec92a3dd533
Marc raphael <marcraphael979@gmail.com>
2023-04-27 10:17:10 +0100

    Sould return false when can_use_service? is called on a teacher

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -10,4 +10,10 @@ describe Teacher do
       expect(@teacher).to be_instance_of(Teacher)
     end
   end
+
+  context 'when can_use_service? method is called' do
+    it 'return true or false' do
+      expect(@teacher.can_use_services?).not_to be false
+    end
+  end
 end
