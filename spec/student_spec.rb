14b5a8ae16bd7d2ec14209dc23619e6d2940c592
Marc raphael <marcraphael979@gmail.com>
2023-04-27 10:03:16 +0100

    Fix error on student test cases

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,73 +1,43 @@
-# require './student'
+require './student'
+require_relative '../rental'
 
-# describe Student do
-#   before(:each) do
-#     @parent_permission = true
-#     @student = Student.new(1, 16, 'John', @parent_permission)
-#   end
+describe Student do
+  before(:each) do
+    @parent_permission = true
+    @student = Student.new(1, 'John', 16, @parent_permission)
+  end
 
-#   context 'creating a student' do
-#     it 'returns the student age' do
-#       expect(@student.age).to eq 16
-#     end
+  context 'creating a student' do
+    it 'returns the student age' do
+      expect(@student.age).to eq 16
+    end
 
-#     it 'returns the student name' do
-#       expect(@student.name).to eq 'John'
-#     end
+    it 'returns the student name' do
+      expect(@student.name).to eq 'John'
+    end
 
-#     it 'returns true for can_use_services? when the student has parent permission' do
-#       expect(@student.can_use_services?).to eq true
-#     end
+    it 'returns true for can_use_services? when the student has parent permission' do
+      expect(@student.can_use_services?).to eq true
+    end
 
-#     it 'returns false for can_use_services? when the student does not have parent permission and is underage' do
-#       student = Student.new(2, 16, 'Jane', false)
-#       expect(student.can_use_services?).to eq false
-#     end
+    it 'returns false for can_use_services? when the student does not have parent permission and is underage' do
+      student = Student.new(2, 'Jane', 16, false)
+      expect(student.can_use_services?).to eq false
+    end
 
-#     it 'returns true for can_use_services? when the student is of age and does not have parent permission' do
-#       student = Student.new(3, 18, 'Mike', false)
-#       expect(student.can_use_services?).to eq true
-#     end
-#   end
+    it 'returns true for can_use_services? when the student is of age and does not have parent permission' do
+      student = Student.new(3, 'Mike', 18, false)
+      expect(student.can_use_services?).to eq true
+    end
+  end
 
-#   context 'managing rentals' do
-#     before(:each) do
-#       @book = Book.new('Title', 'Author')
-#       @rental = Rental.new(@book, @student)
-#     end
+  context 'miscellaneous' do
+    it 'returns a shrug emoji when the student plays hooky' do
+      expect(@student.play_hooky).to eq '¯\(ツ)/¯'
+    end
 
-#     it 'allows a student to rent a book' do
-#       expect(@student.rentals).to include(@rental)
-#       expect(@book.is_available?).to eq false
-#     end
-
-#     it 'allows a student to return a rented book' do
-#       @student.return_book(@book)
-#       expect(@student.rentals).not_to include(@rental)
-#       expect(@book.is_available?).to eq true
-#     end
-
-#     it 'raises an error when a student tries to return a book they have not rented' do
-#       book2 = Book.new('Another Title', 'Another Author')
-#       expect { @student.return_book(book2) }.to raise_error('This student did not rent this book')
-#     end
-#   end
-
-#   context 'classroom' do
-#     it 'adds the student to the classroom if a classroom is provided' do
-#       classroom = Classroom.new('History')
-#       student = Student.new(4, 16, 'Sarah', @parent_permission, classroom)
-#       expect(classroom.students).to include(student)
-#     end
-#   end
-
-#   context 'miscellaneous' do
-#     it 'returns a shrug emoji when the student plays hooky' do
-#       expect(@student.play_hooky).to eq '¯\(ツ)/¯'
-#     end
-
-#     it 'notifies upon student creation' do
-#       expect { Student.new(5, 17, 'Alice', @parent_permission) }.to output("Student Alice enrolled\n").to_stdout
-#     end
-#   end
-# end
+    it 'notifies upon student creation' do
+      expect { Student.new(5, 'Alice', 17, @parent_permission) }.to output("Student Alice enrolled\n").to_stdout
+    end
+  end
+end
